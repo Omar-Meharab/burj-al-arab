bf3169ffede74129265f09eab465cd9c248d6ea7
Omar Meharab <omarmeharab81@gmail.com>
2021-03-31 02:02:53 +0600

    a

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAsihSADFnF_FoZ6je8p1W9lilyqX7cHWQ",
-    authDomain: "burj-al-arab.firebaseapp.com",
-    databaseURL: "https://burj-al-arab.firebaseio.com",
-    projectId: "burj-al-arab",
-    storageBucket: "burj-al-arab.appspot.com",
-    messagingSenderId: "244522202387",
-    appId: "1:244522202387:web:f3801d0588387140e1d6e6"
-  };
-  export default firebaseConfig;
+  apiKey: "AIzaSyDomnTNtc-RxfZrKmrSeJMjBTvPxCUCjqk",
+  authDomain: "burj-al-arab-7952c.firebaseapp.com",
+  projectId: "burj-al-arab-7952c",
+  storageBucket: "burj-al-arab-7952c.appspot.com",
+  messagingSenderId: "643966727424",
+  appId: "1:643966727424:web:6f621e4ea7bea40dfaafe5"
+};
+
+export default firebaseConfig;
